314b5983a1b2078e6ba040123e166787310337b5
DANIEL MONDRAGÓN HUERTA <dmondragon@posgrado.cidesi.edu.mx>
2024-10-14 11:53:59 -0600

    Agregar proyecto CCS inicial

---
+++ main.c
@@ -0,0 +1,17 @@
+
+
+/**
+ * main.c
+ */
+int var0 = 0;
+int var1 = 0;
+int var2 = 0;
+float promedio = 0.0;
+int main(void)
+{
+    while(1)
+    {
+        promedio = (var0+var1+var2)*0.333;
+    }
+	return 0;
+}
